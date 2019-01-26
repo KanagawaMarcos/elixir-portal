b4046b41340060c21cd82315a3b06882448faf2f
Marcos Costa <mcs1901@outlook.com>
2019-01-25 22:21:06 -0300

    fn -> Inspecting portals with protocols

--- lib/portal.ex
+++ lib/portal.ex
@@ -1,18 +1,30 @@
 defmodule Portal do
-  @moduledoc """
-  Documentation for Portal.
-  """
+  defstruct [:left, :right]
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Portal.hello()
-      :world
-
+  Start transfering 'data from 'left to 'right'
   """
-  def hello do
-    :world
+  def transfer(left,right,data)do
+    for item <- data do
+      Portal.Door.push(left,item)
+    end
+
+    # Returns a portal struct we will use next
+    %Portal{left: left, right: right}
+  end
+
+  @doc """
+  pushes data to the right in the given 'portal'.
+  """
+  def push_right(portal)do
+    # See if we can pop data from left, if so, push the
+    # popped data to the right. Otherwise, do nothing.
+    case Portal.Door.pop(portal.left)do
+      :error -> :ok
+      {:ok, h} -> Portal.Door.push(portal.right, h)
+
+    end
+    # let's run the portal itself
+    portal
   end
 end
